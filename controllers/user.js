0ae9c9a06580ae2baac4c233fe589fa27407d5c6
Mohammad <spystar0003@gmail.com>
2023-11-30 18:40:01 +0330

    add password rewinder --- 2

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const axios = require('axios');
+const jwt = require('jsonwebtoken');
 
 const user = require('../models/user');
 const { mailSender } = require('../utils/mailer');
@@ -106,8 +107,15 @@ exports.forgetPassPost = async (req, res) => {
     const foundedUser = await user.findOne({email : req.body.email})
     
     if(foundedUser === null) {
-        return res.redirect("/")
+        return res.render("forgetPass", {
+            pageTitle : "Remind Password",
+            path : '/forget-password',
+            msg : "There is no user with that name !"
+        })
     }
+
+    const token = jwt.sign({id : foundedUser._id}, process.env.JWT_SECRET, {expiresIn : '1h'})
+    const resetLink = `http://localhost:3000/user/pass-rewinder/${token}`
 
     mailSender(
         foundedUser.email,
@@ -116,7 +124,7 @@ exports.forgetPassPost = async (req, res) => {
         `
             <h1> Let's set a new Password </h1>
             <p> Click on the below link : </p>
-            <a href="http://localhost:3000/user/pass-rewinder"> Set New PassWord </a>
+            <a href=${resetLink}> Set New Password </a>
         `
     )
 
@@ -129,8 +137,52 @@ exports.forgetPassPost = async (req, res) => {
 
 exports.passRewinder = async (req, res) => {
 
+    const token = req.params.token
+
+    let decodedToken;
+
+    try {
+        decodedToken = await jwt.verify(token,process.env.JWT_SECRET)
+    } catch (err) {
+        console.log(err)
+        return res.redirect("/404")
+    }
+
     res.render("rewinder", {
         pageTitle : "Password Rewinder",
-        path : "/pass-rewinder"
+        path : "/pass-rewinder",
+        userId : decodedToken.id
+    })
+}
+
+exports.passModifier = async (req, res) => {
+
+    let { password, repeatPassword } = req.body
+    const { id } = req.params
+    
+
+    if(password !== repeatPassword) {
+
+        req.flash("error" , "Password isn't like Repeat Password !")
+
+        res.render("rewinder", {
+            pageTitle : "Password Rewinder",
+            path : "/pass-rewinder",
+            errors : req.flash("error"),
+            userId : id
+        })
+    }
+
+    await bcrypt.hash(password, 10 , async (err, hash) => {
+        try {
+            await user.findByIdAndUpdate(id, {
+                password : hash,
+                repeatPassword : hash
+            })
+            res.redirect("/user/login")
+        } catch (err) {
+            console.log(err)
+            res.redirect("/404")
+        }
     })
 }
--- routes/sign.js
+++ routes/sign.js
@@ -19,6 +19,8 @@ router.get("/forget-password", controller.forgetPass)
 
 router.post("/forget-password", controller.forgetPassPost)
 
-router.get("/pass-rewinder", controller.passRewinder)
+router.get("/pass-rewinder/:token", controller.passRewinder)
+
+router.post("/pass-modifier/:id", controller.passModifier)
 
 module.exports = router
